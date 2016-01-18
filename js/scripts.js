e505cd90d1b1bcf411f8f7465138bc1d81d63d58
eddieduro <student@epicodus.com>
2016-01-18 12:05:13 -0800

    user input now returns which type of triangle they created

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,23 @@
 var triangleTracker = function(side1, side2, side3) {
-  if (side1 + side2 <= side3){
-    return true;
+  if ((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side3 + side1 <= side2)){
+    $("#isoceles").show();
   } else if ((side1 === side2) && (side2 === side3)){
-    return true;
-  } else {
-  return false;
+    $("#equilateral").show();
+  } else if ((side1 !== side2) && (side2 !== side3)){
+    $("#scalene").show();
   }
+
 };
+
+$(document).ready(function() {
+  $('#triangle-tracker').submit(function(event){
+    var side1 = $('input#side1').val();
+    var side2 = $('input#side2').val();
+    var side3 = $('input#side3').val();
+
+    var results = triangleTracker(side1, side2, side3);
+
+    event.preventDefault();
+  });
+
+});
