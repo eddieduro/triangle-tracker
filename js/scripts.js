4be66ff4154a85d87b8ddd3ee9efdff130df2108
eddieduro <student@epicodus.com>
2016-01-18 15:26:23 -0800

    functional triangle testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,17 @@
 var triangleTracker = function(side1, side2, side3) {
-  if ((side1 + side2 <= side3) || (side2 + side3 <= side1) || (side3 + side1 <= side2)){
+ if ((side1 === side2) && (side2 !== side3) || (side3 === side1) && (side2 !== side1)) {
     $("#isoceles").show();
-  } else if ((side1 === side2) && (side2 === side3)){
+    return true;
+  } else if ((side1 === side2) && (side2 === side3) && (side3 === side1)){
     $("#equilateral").show();
-  } else if ((side1 !== side2) && (side2 !== side3)){
+    return true;
+  } else if ((side1 !== side2) && (side2 !== side3)) {
     $("#scalene").show();
+    return true;
+  }  else if ((side1 <= side3) && (side2 <= side3) || (side3 <= side1) && (side2 <= side1) || (side1 <= side2) && (side3 <= side2)){
+      $('p#error').append('<p>try again</p>');
+      return true;
   }
-
 };
 
 $(document).ready(function() {
@@ -19,5 +24,4 @@ $(document).ready(function() {
 
     event.preventDefault();
   });
-
 });
--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,14 @@
 describe('triangleTracker', function() {
-  it("is false for a triangle that has no sides equal", function () {
-    expect(triangleTracker(1, 3, 2)).to.equal(false);
+  it("is true for a triangle that has no sides equal", function () {
+    expect(triangleTracker(1, 2 , 4)).to.equal(true);
   });
 
   it("is true for a triangle that has 2 sides that are less than or equal to the 3rd side", function() {
     expect(triangleTracker(2,2,4)).to.equal(true);
+  });
+
+  it("is true for a trianlge that has  exactly 2 equal sides", function () {
+    expect(triangleTracker(2,2,1)).to.equal(true);
   });
 
   it("is true for a triangle that has 3 equal sides", function() {
